b1943657d0cae9437656c52d56fa04644f44b6fa
Manthan Gupta <manthangupta109@gmail.com>
2024-02-17 15:21:39 +0530

    REPL

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"CricLang/repl"
+	"fmt"
+	"os"
+	"os/user"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Welcome %s to CricLang: A toy programming language for cricket enthusiasts!\n", user.Username)
+	repl.Start(os.Stdin, os.Stdout)
+}
