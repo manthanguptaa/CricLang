765b58e06b31e6fe3955e589292799ff25c9ff22
Manthan Gupta <manthangupta109@gmail.com>
2024-02-18 10:01:20 +0530

    Feat: parsing integer literals not supported

--- parser/parser.go
+++ parser/parser.go
@@ -5,6 +5,7 @@ import (
 	"CricLang/lexer"
 	"CricLang/token"
 	"fmt"
+	"strconv"
 )
 
 const (
@@ -38,6 +39,7 @@ func New(l *lexer.Lexer) *Parser {
 
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 
 	// reading 2 tokens together so curToken and peekToken are both set
 	p.nextToken()
@@ -129,6 +131,19 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 
 func (p *Parser) parseIdentifier() ast.Expression {
 	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.curToken}
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("couldn't parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+	lit.Value = value
+	return lit
 }
 
 func (p *Parser) curTokenIs(t token.TokenType) bool {
