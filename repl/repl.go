1ad8ee5827eca5f6c3ccfd7b8a74046de709ad82
Manthan Gupta <manthangupta109@gmail.com>
2024-02-19 14:29:39 +0530

    Feat: adding evaluation step to the REPL

--- repl/repl.go
+++ repl/repl.go
@@ -1,6 +1,7 @@
 package repl
 
 import (
+	"CricLang/evaluator"
 	"CricLang/lexer"
 	"CricLang/parser"
 	"bufio"
@@ -29,8 +30,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
