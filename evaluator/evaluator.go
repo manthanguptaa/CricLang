006caf7e9a63f2f6850a978ca463d07ff78ad188
Manthan Gupta <manthangupta109@gmail.com>
2024-02-19 15:04:24 +0530

    Feat: evaluation of boolean suppoted

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,11 @@ import (
 	"CricLang/object"
 )
 
+var (
+	NOT_OUT = &object.Boolean{Value: true}
+	OUT     = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
@@ -13,6 +18,8 @@ func Eval(node ast.Node) object.Object {
 		return Eval(node.Expression)
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
 	}
 	return nil
 }
@@ -24,4 +31,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
 		result = Eval(statement)
 	}
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return NOT_OUT
+	}
+	return OUT
 }
