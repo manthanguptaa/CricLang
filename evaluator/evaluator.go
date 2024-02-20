a9722994e606795b59ddc65d069eb9da41659eb2
Manthan Gupta <manthangupta109@gmail.com>
2024-02-20 14:29:03 +0530

    Feat: support for string concatenation

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -152,6 +152,8 @@ func evalInfixExpression(operator string, left, right object.Object) object.Obje
 	switch {
 	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
 		return evalIntegerInfixExpression(operator, left, right)
+	case left.Type() == object.STRING_OBJ && right.Type() == object.STRING_OBJ:
+		return evalStringInfixExpression(operator, left, right)
 	case left.Type() != right.Type():
 		return newMisfield("player type mismatch: %s %s %s", left.Type(), operator, right.Type())
 	case operator == "==":
@@ -274,4 +276,14 @@ func unwrapSignalDecisionValue(obj object.Object) object.Object {
 		return signalDecisionValue.Value
 	}
 	return obj
+}
+
+func evalStringInfixExpression(operator string, left, right object.Object) object.Object {
+	if operator != "+" {
+		return newMisfield("unknown operator team: %s %s %s", left.Type(), operator, right.Type())
+	}
+
+	leftVal := left.(*object.String).Value
+	rightVal := right.(*object.String).Value
+	return &object.String{Value: leftVal + rightVal}
 }
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -198,6 +198,7 @@ func TestMisfieldHandling(t *testing.T) {
 				signaldecision 1;
 			}`, "unknown operator team: BOOLEAN + BOOLEAN"},
 		{"foobar", "identifier not found: foobar"},
+		{`"Hello" - "Hello"`, "unknown operator team: STRING - STRING"},
 	}
 
 	for _, tt := range tests {
@@ -293,6 +294,20 @@ func TestStringLiteral(t *testing.T) {
 	str, ok := evaluated.(*object.String)
 	if !ok {
 		t.Fatalf("object isn't String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
 	}
 
 	if str.Value != "Hello World!" {
